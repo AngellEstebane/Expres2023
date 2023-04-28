a0e8fed0d281f7586ed265cc0049771cf3398aa2
AngellEstebane <AngellEstebane@gmail.com>
2023-04-28 09:56:28 -0600

    Servidor con bodyparser

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ app.listen(3000, function() {
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
 });
 
+// require packages and set the port
+const express = require('express');
+const port = 3002;
+
+//para permitir manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes')
+
+// Usar node is body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+
+    extended: true,
+}));
+
+routes(app);
+
+// iniciar el servidor
+const server = app.listen(port, (error) => {
+    if (error) return console.log('Error: ${error}');
+
+    console.log('El servidor escucha en el puerto ${server.address().port}');
+});
