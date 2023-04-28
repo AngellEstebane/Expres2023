0aa6fe464b5ebbc2141463b40e281460f3d89aff
AngellEstebane <AngellEstebane@gmail.com>
2023-04-28 09:28:17 -0600

    Manejo de rutas

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('hola mundo');
+    res.send('Una API basica desde Express');
 });
+
+
+app.get('/', function(req, res) {
+    res.send('Hola desde la API');
+});
+
+app.get('/', function(req, res) {
+    res.send('Adios desde de una API');
+});
+
 
 app.listen(3000, function() {
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
 });
+
