9a83d9ac9bfb3b8785b8fad391bd397d2eb84567
AngellEstebane <AngellEstebane@gmail.com>
2023-05-03 09:55:44 -0600

    Conexion base de datos

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res) {
-    res.send('Una API desde Express');
-});
-
-
-app.get('/saludo', function(req, res) {
-    res.send('Hola desde la API');
-});
-
-app.get('/despedida', function(req, res) {
-    res.send('Adios desde de una API');
-});
-
-
-app.listen(3000, function() {
-    console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
-});
-
-// require packages and set the port
+//Require packages and set the port
 const express = require('express');
 const port = 3002;
-
 //para permitir manejo de POST y PUT
 const bodyParser = require('body-parser');
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
+const app = express();
 
-// Usar node is body parsing middleware
+//Usar Node.js body parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-
     extended: true,
 }));
 
 routes(app);
 
-// iniciar el servidor
-const server = app.listen(port, (error) => {
-    if (error) return console.log('Error: ${error}');
+//Iniciar el servidor
+const server = app.listen(port, (error)=> {
+    if(error) return console.log(`Error: ${error}`);
 
-    console.log('El servidor escucha en el puerto ${server.address().port}');
-});
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
+})
