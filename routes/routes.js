b8516d90feca70d331d635ec4963ba8012700d77
AngellEstebane <AngellEstebane@gmail.com>
2023-05-08 09:27:50 -0600

    Agregar un nuevo usuario

--- routes/routes.js
+++ routes/routes.js
@@ -30,3 +30,13 @@ app.get('/users/:id', (request, response) => {
     response.send(result);
   });
 });
+
+//Agregar un nuevo usuario
+app.post('/users', (request, response) => {
+  post.query('INSERT INTO users SET ?', request.body, (error, result) => {
+    if (error) throw error;
+
+    response.status(201).send('User added with ID: ${result.insertId}' );
+  });
+});
+
